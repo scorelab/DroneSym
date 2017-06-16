312f12348ef8e76a0fc9cf1994d35e5a604c5cb6
Hasanga Somaratne <hasanga.charaka@gmail.com>
2017-06-17 00:48:38 +0530

    Integrate google maps

--- dronesym-frontend/src/app/app.component.ts
+++ dronesym-frontend/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  centerCoords = {
+    lat: 6.9023,
+    lng: 79.8613
+  };
 }
--- dronesym-frontend/src/app/app.module.ts
+++ dronesym-frontend/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+    	apiKey: 'YOUR_API_KEY_HERE'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
