4de0e5d4baaf08a6357e2d473185c78e6af0afac
Hasanga Somaratne <hasanga.charaka@gmail.com>
2017-07-26 23:03:22 +0530

    Remove unnecessary takeoff command

--- dronesym-python/flask-api/src/mavparser.py
+++ dronesym-python/flask-api/src/mavparser.py
@@ -5,8 +5,6 @@ def create_mission(drone, waypoints):
 	cmds = drone.commands
 	cmds.clear()
 
-	cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, waypoints[0]['lat'], waypoints[0]['lon'], 10))
-
 	for (i, wp) in enumerate(waypoints):
 		cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT, mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, wp['lat'], wp['lon'], 10))
 
