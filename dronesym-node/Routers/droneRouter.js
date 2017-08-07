48a2f91a7b37dccf08482ce05e7ab1521add44b6
Hasanga Somaratne <hasanga.charaka@gmail.com>
2017-08-07 21:14:03 +0530

    Add drone group handling routes

--- dronesym-node/Routers/droneRouter.js
+++ dronesym-node/Routers/droneRouter.js
@@ -63,4 +63,36 @@ router.post('/resume/:id', authenticate, authorize(['admin', 'user']), function(
 		res.json(status);
 	});
 })
+
+router.post('/groups/create', authenticate, authorize(['admin']), function(req, res){
+	drones.createGroup(req.body.name, req.user.id, function(status){
+		res.json(status);
+	})
+})
+
+router.post('/groups/remove/:id', authenticate, authorize(['admin']), function(req, res){
+	drones.removeGroup(req.params.id, function(status){
+		res.json(status);
+	})
+})
+
+router.get('/groups', authenticate, function(req, res){
+	drones.getGroups(req.user.id, function(status){
+		res.json(status);
+	})
+})
+
+
+router.post('/groups/:id/add', authenticate, function(req, res){
+	drones.addToGroup(req.params.id, req.body.drones, function(status){
+		res.json(status);
+	})
+})
+
+router.post('/groups/:id/remove/:droneId', authenticate, function(req, res){
+	drones.removeFromGroup(req.params.id, req.params.droneId, function(status){
+		res.json(status);
+	})
+})
+
 module.exports = router;
