b237f023b96cdc4a9762ec869cff17e9622340cf
Jakub Mularski <jakubmularski723@gmail.com>
2018-10-30 13:36:19 +0100

    Tests for remove drone in droneCtrl

--- dronesym-node/tests/droneCtrl.js
+++ dronesym-node/tests/droneCtrl.js
@@ -3,7 +3,7 @@ let assert = require("assert");
 let randomstring = require("randomstring");
 let randomlocation = require("random-location");
 
-let {createDrone, getDroneIds} = require("../Controllers/droneCtrl");
+let {createDrone, getDroneIds, removeDrone} = require("../Controllers/droneCtrl");
 
 function generateDroneName() {
     return randomstring.generate(10);
@@ -57,6 +57,41 @@ describe("DRONE CONTROLLER", () => {
                 const loc = {"lat": 52.237049};
                 createDrone(name, loc, "597073ad587a6615c459e2bf", function(response){
                     assert.strictEqual(response.status, "ERROR");
+                    done();
+                });
+            });
+        });
+    });
+    describe("Remove drone", () => {
+        let droneId;
+        beforeEach( (done) => {
+            const name = generateDroneName();
+            const loc = generateDroneLoc();
+            createDrone(name, loc, "597073ad587a6615c459e2bf", function(response){
+                getDroneIds((result) => {
+                    droneId = result[result.length - 1];
+                    done();
+                });
+            });
+        })
+        describe("Happy path", () => {
+            it("Contains all needed params and drone not flying", (done) => {
+                removeDrone(droneId, "IDLE", (result) => {
+                    assert.strictEqual(result.status, "OK");
+                    done();
+                });
+            });
+        });
+        describe("Should throw errors", () => {
+            it("Drone is flying", (done) => {
+                removeDrone(droneId, "FLYING", (result) => {
+                    assert.strictEqual(result.status, "ERROR");
+                    done();
+                });
+            });
+            it("Doesn't contain droneId", (done) => {
+                removeDrone("", "IDLE", (result) => {
+                    assert.strictEqual(result.status, "ERROR");
                     done();
                 });
             });
