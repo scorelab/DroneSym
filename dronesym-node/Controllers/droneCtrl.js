08b8369f3aaa2d1dcbd2a24d233afc1a716ac257
Jakub Mularski <jakubmularski723@gmail.com>
2018-11-06 00:47:08 +0100

    docs(jsdoc): added basic JSDoc for every method of controller

--- dronesym-node/Controllers/droneCtrl.js
+++ dronesym-node/Controllers/droneCtrl.js
@@ -8,6 +8,11 @@ var droneRef = db.ref('/drones');
 
 var flaskUrl = 'http://localhost:5000/dronesym/api/flask';
 
+/**
+ * Function to send drone data to browser of user
+ * @param {object} snapshot - object containing information about every drone 
+ * @param {string} socket - id of socket
+ */
 var sendSnapsot = function(snapshot,socket){
   var array = [];
   var userId = socket.decoded_token.id;
@@ -51,6 +56,13 @@ if(process.env.NODE_ENV !== "test"){
 }
 //Send update drone data upon change to firebase
 
+/**
+ * Creates drone with given data
+ * @param {string} name - name of drone
+ * @param {object} location - object containing lat and lon values
+ * @param {string} userId - id of person who is owner of that drone
+ * @param {function} callBack - function to return result of creating drone
+ */
 exports.createDrone = function(name, location, userId, callBack){
 	if(!name  || name === ''){
 		callBack({ status : "ERROR", msg: "Drone name is required"});
@@ -70,6 +82,12 @@ exports.createDrone = function(name, location, userId, callBack){
 	})
 }
 
+/**
+ * Removes drone from firebase database and group drones
+ * @param {string} droneId - id of drone
+ * @param {string} droneStatus - status of drone (can't remove drone in middle of flight)
+ * @param {function} callBack - function to return result of removing drone to
+ */
 exports.removeDrone = function(droneId, droneStatus, callBack){
 	if(droneStatus === "FLYING"){
 		callBack({ status : "ERROR", msg : "Drone in flight"});
@@ -108,6 +126,12 @@ exports.removeDrone = function(droneId, droneStatus, callBack){
 	})
 }
 
+/**
+ * Creates a new group
+ * @param {string} groupName - unique name of group
+ * @param {string} userId - id of owner of that group
+ * @param {function} callBack - function to return result of creating group to
+ */
 exports.createGroup = function(groupName, userId, callBack){
 	if(!groupName || groupName === ""){
 		callBack( { status : "ERROR", msg: "Group name must be specified"});
@@ -136,6 +160,11 @@ exports.createGroup = function(groupName, userId, callBack){
 	})
 }
 
+/**
+ * Removes a group
+ * @param {string} groupId - id of owner of that group
+ * @param {function} callBack - function to return result of removing group to
+ */
 exports.removeGroup = function(groupId, callBack){
 	Group.findOneAndRemove({ _id : groupId }, function(err, group){
 		if(err){
@@ -154,6 +183,11 @@ exports.removeGroup = function(groupId, callBack){
 	});
 }
 
+/**
+ * Gets list of groups of user 
+ * @param {string} userId - id of user
+ * @param {function} callBack - function to return result of getting group to
+ */
 exports.getGroups = function(userId, callBack){
 	Group.find({ userId : userId }, function(err, groups){
 		if(err){
@@ -164,6 +198,12 @@ exports.getGroups = function(userId, callBack){
 	});
 }
 
+/**
+ * Adds drones to group 
+ * @param {string} groupId - id of group
+ * @param {array} drones - array of drone objects
+ * @param {function} callBack - function to return result of adding drones to
+ */
 exports.addToGroup = function(groupId, drones, callBack){
 	Group.findOneAndUpdate({ _id : groupId }, { $push : { drones : { $each : drones }}}, { new : true }, function(err, group){
 		if(err){
@@ -174,6 +214,12 @@ exports.addToGroup = function(groupId, drones, callBack){
 	});
 }
 
+/**
+ * Removes drone with given ids from group 
+ * @param {string} groupId - id of group
+ * @param {string} droneId - id of drone
+ * @param {function} callBack - function to return result of removing drone to
+ */
 exports.removeFromGroup = function(groupId, droneId, callBack){
 	Group.findOneAndUpdate({ _id : groupId }, { $pull : { drones : droneId }}, { new : true }, function(err, group){
 		if(err){
@@ -185,6 +231,12 @@ exports.removeFromGroup = function(groupId, droneId, callBack){
 	});
 }
 
+/**
+ * Updates waypoints of given drone 
+ * @param {string} id - id of drone
+ * @param {array} waypoints - array of locations - lat, lon objects
+ * @param {function} callBack - function to return result of adding waypoints to
+ */
 exports.updateWaypoints = function(id, waypoints, callBack){
   var waypointsRef = droneRef.child(id).child('waypoints');
 
@@ -197,6 +249,10 @@ exports.updateWaypoints = function(id, waypoints, callBack){
   });
 }
 
+/**
+ * Gets drone ids of all drones
+ * @param {function} callBack - function to return result of getting drone ids to
+ */
 exports.getDroneIds = function(callBack){
 	var drones = [];
 
@@ -209,6 +265,12 @@ exports.getDroneIds = function(callBack){
 	});
 }
 
+/**
+ * Updates drone status 
+ * @param {string} id - id of drone
+ * @param {string} status - status to update drone to, every update has its timestamp
+ * @param {function} callBack - function to return result of updating drone status to
+ */
 exports.updateDroneStatus = function(id, status, callBack){
 	var timestamp = new Date();
 	status["timestamp"] = timestamp.valueOf();
@@ -221,6 +283,11 @@ exports.updateDroneStatus = function(id, status, callBack){
 	});
 }
 
+/**
+ * Gets drone params by its id 
+ * @param {string} id - id of drone
+ * @param {function} callBack - function to return result of getting drone params to
+ */
 exports.getDroneById = function(id, callBack){
 	if(!id || id === ""){
 		callBack({ status : "ERROR", msg : "Id must be specified"});
@@ -233,6 +300,12 @@ exports.getDroneById = function(id, callBack){
 	});
 }
 
+/**
+ * Takeoffs drone on journey of several waypoints 
+ * @param {string} id - id of drone
+ * @param {array} waypoints - array of objects containing lat lon points, that drone will fly towards
+ * @param {function} callBack - function to return result of takeoffing drone to
+ */
 exports.takeoffDrone = function(id, waypoints, callBack){
 	var waypoints = waypoints || [];
 
@@ -243,12 +316,22 @@ exports.takeoffDrone = function(id, waypoints, callBack){
 	);
 }
 
+/**
+ * Lands drone, stops its flight
+ * @param {string} id - id of drone
+ * @param {function} callBack - function to return result of updating drone status to
+ */
 exports.landDrone = function(id, callBack){
 	request.post(`${flaskUrl}/${id}/land`, function(err, response, body){
 		callBack(body);
 	});
 }
 
+/**
+ * Resumes flight of drone 
+ * @param {string} id - id of drone
+ * @param {function} callBack - function to return result of resuming drone flight to
+ */
 exports.resumeFlight = function(id, callBack){
 	console.log('resuming...');
 	request.post(`${flaskUrl}/${id}/resume`, function(err, response, body){
