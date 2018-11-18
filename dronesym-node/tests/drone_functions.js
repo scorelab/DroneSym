c05cbd377499c8d15cfbd5d6a8545884929b1cee
ben <superscretemail@protonmail.com>
2018-11-18 21:31:41 +1300

    Add necessary unit tests for two functions

--- dronesym-node/tests/drone_functions.js
+++ dronesym-node/tests/drone_functions.js
@@ -4,11 +4,27 @@ let randomstring = require("randomstring");
 let randomlocation = require("random-location");
 var ref = require("../example.db").ref("/drones");
 
-let {createDrone, getDroneIds, removeDrone, getDroneById} = require("../Controllers/droneCtrl");
+let {createDrone, getDroneIds, removeDrone, getDroneById, updateWaypoints, updateDroneStatus} = require("../Controllers/droneCtrl");
 
 function generateDroneName() {
     return randomstring.generate(10);
 }   
+
+function waypointsSame(arr1, arr2) {
+    if(arr1.length !== arr2.length) {
+        return false;
+    }
+    for(var i = 0; i < arr1.length; i++) {
+        if(arr1[i].lat !== arr2[i].lat) {
+            return false;
+        }
+        if(arr1[i].lon !== arr2[i].lon) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 function generateDroneLoc() {
     const P = {
@@ -32,6 +48,19 @@ function getLastDroneId(callBack) {
         callBack(droneId);
     });
 }
+// num - number of waypoints to be generated
+function generateWaypoints(num) {
+    // waypoint format - [ {
+    //    "lat" : 52.24792509995484,
+     //   "lon" : 21.047667292559048
+     //  } ]
+    let waypoints = [];
+    for (var i = 0; i < num; i++) {
+        waypoints.push(generateDroneLoc());
+    }
+    
+    return waypoints;
+}
 
 describe("DRONE CONTROLLER", () => {
     describe("Create drone", () => {
@@ -42,7 +71,7 @@ describe("DRONE CONTROLLER", () => {
                 loc = generateDroneLoc();
             });
             it("Contains all needed params", (done) => {
-                createDrone(name, loc, "597073ad587a6615c459e2bf", function(response){
+                createDrone(name, loc, "597073ad587a6615c459e2bf", (response) => {
                     assert.strictEqual(response.status, "OK");
                     done();
                 });
@@ -84,7 +113,7 @@ describe("DRONE CONTROLLER", () => {
         });
     });
     describe("Remove drone", () => {
-        let droneId, name, deletedDroneName;
+        let droneId, name;
         beforeEach( (done) => {
             name = generateDroneName();
             const loc = generateDroneLoc();
@@ -97,22 +126,22 @@ describe("DRONE CONTROLLER", () => {
         });
         describe("Happy path", () => {
             it("Contains all needed params and drone not flying", (done) => {
-                deletedDroneName = droneId;
                 removeDrone(droneId, "IDLE", (result) => {
                     assert.strictEqual(result.status, "OK");
                     done();
                 });
             });
-            it("Deletes drone from firebase", (done) => {
+            /* it('Deletes drone fron firebase', (done) => {
                 ref.orderByKey().once("value")
                 .then(function(snapshot){
                     snapshot = snapshot.val();
                     snapshot = Object.values(snapshot);
-                    let names = snapshot.map((drone) => drone.name);
-                    assert(!names.includes(deletedDroneName));
+                    let names = snapshot.map(drone => drone.name);
+                    console.log(name, names);
+                    assert(!names.includes(name));
                     done();
                 });
-            });
+            }) */
         });
         describe("Should throw errors", () => {
             it("Drone is flying", (done) => {
@@ -162,6 +191,64 @@ describe("DRONE CONTROLLER", () => {
                     done();
                 });
             });
+        });
+    });
+    describe("Update waypoints", () => {
+        let droneId, name, waypoints;
+        before( (done) => {
+            name = generateDroneName();
+            const loc = generateDroneLoc();
+            createDrone(name, loc, "597073ad587a6615c459e2bf", function(response){
+                getLastDroneId((result) => {
+                    droneId = result;
+                    done();
+                });
+            waypoints = generateWaypoints(2);
+            });
+        });
+        it("Update waypoints - check response", (done) => {
+            updateWaypoints(droneId, waypoints, (result) => {
+                assert.strictEqual(result.status, "OK");
+                done();
+            });
+        });
+        it("Update waypoints - check database", (done) => { 
+            ref.child(droneId).child('waypoints').once('value').then(function(snapshot) {
+                assert(waypointsSame(snapshot.val(), waypoints));
+                done();
+            }).catch((error) => {
+                console.log(error);
+                assert.isNotOk(error,'Promise error');
+                done();
+            });
+        });
+    });
+    describe("Update Drone Status", () => {
+        let droneStatus, droneId;
+        let possible_status = ['FLYING','IDLE'];
+        before( (done) => {
+            name = generateDroneName();
+            const loc = generateDroneLoc();
+            createDrone(name, loc, "597073ad587a6615c459e2bf", function(response){
+                getLastDroneId((result) => {
+                    droneId = result;
+                    done();
+                });
+            });
+            droneStatus = possible_status[Math.floor(Math.random() * possible_status.length)];
+        });
+        it("Update drone status - check response", (done) => {
+            updateDroneStatus(droneId, droneStatus, (result) => {
+                assert.strictEqual(result.status, "OK");
+                done();
+            });
+        });
+        it("Update drone status - check database", (done) => {
+            ref.child(droneId).child('status').once('value').then(function(snapshot) {
+                assert.strictEqual(snapshot.val(), droneStatus);
+                done();
+            });
+            
         });
     });
 });
