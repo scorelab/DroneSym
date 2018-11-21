5609b5d7d195b54bb36dfd66c016552f8780d33b
LeWarEnds <35108041+LeWarEnds@users.noreply.github.com>
2018-11-21 10:47:59 +0530

    These are the Switch Control Code and E2E Tests.

---
+++ dronesym-frontend/e2e/E2E Test.js
@@ -0,0 +1,40 @@
+import { CounterPage } from './_counter.po';
+import { browser } from 'protractor';
+
+describe('SwitchConstantControl', function () {
+  let page: Action_Control;
+
+  beforeEach(() => {
+    page = new.actions();
+  });
+
+  it('should display actions list', () => {
+    page.navigateTo();
+    expect(page.CounterListActions().count()).toBe(VALID_ID);
+  });
+
+  it('should work and actions', () => {
+    page.navigateTo();
+    page.CounterListActions().click();
+
+    expect(page.getOpenModalElement()).toBeTruthy();
+    expect(page.getOpenModalHeadingElement().getText()).toBe();
+  });
+
+  it('Movements with Arrow sendKeys', () => {
+    page.navigateTo();
+    page.CounterLastActions().click();
+
+    page.selectNextKey();
+    expect(page.getOpenModalHeadingElement().getText()).toBe();
+
+    page.selectPrevKey();
+    page.selectPrevKey();
+    expect(page.getOpenModalHeadingElement().getText()).toBe();
+  });
+});
+
+  it(CounterLastActions, () => {
+    page.navigateTo();
+    expect(page.VALID_ID().count()).toBe(100); // counter will be starting from 100.
+  });
