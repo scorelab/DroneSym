5de616ed7d2007171404b0f922a89c3964934d50
GeethOnion <41124555+GeethOnion@users.noreply.github.com>
2019-03-03 12:03:55 +0530

    Update test.js

--- dronesym-node/tests/test.js
+++ dronesym-node/tests/test.js
@@ -1,12 +1,12 @@
-var https = require("http")
+var https = require("http");
 
 
 try{
 var request = https.get({host:"127.0.0.1:3000/feed"},function(response){
-  console.log(response.statusCode); 
+  Console.log(response.statusCode); 
 });
 
 } catch(e) {
 
-      console.error(e.message);
+      Console.error(e.message);
 }
