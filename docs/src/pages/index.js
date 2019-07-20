cc7b6b396b1533e64d906b0b7929f819bf774c1e
Dilina <nisalya@gmail.com>
2019-07-20 11:23:11 +0530

    Renamed to Docs

---
+++ docs/src/pages/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Documentation</h1>
+    <p>1 - Project Introduction</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
